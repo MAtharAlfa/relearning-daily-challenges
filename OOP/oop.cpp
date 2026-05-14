9e8e67bfd6d88f66b800253e96422c229eec3ffe
MAtharAlfa <alfaathar1311@gmail.com>
2026-05-14 09:33:31 +0700

    feat: encapsulation

--- OOP/oop.cpp
+++ OOP/oop.cpp
@@ -2,16 +2,39 @@
 using std::string;
 
 class Employee { //defaults to private
-public:
+private:
     string Name;
     string Company;
     int Age;
+public:
+    
+    void setName(string name) { //setter
+        if (name.size() > 10) return;
+        Name = name;
+    }
+    string getName() { //getter
+        return Name;
+    }
+    void setCompany(string company) {
+        if(Company == company) return;
+        Company = company;
+    }
+    string getCompany() {
+        return Company;
+    }
+    void setAge(int age) {
+        if (age > 17) //constraint
+        Age = age;
+    }
+    int getAge() {
+        return Age;
+    }
+
     void IntroduceYourself() {
         std::cout << "Name - " << Name << "\n";
         std::cout << "Company - " << Company << "\n";
         std::cout << "Age - " << Age << "\n\n";
     }
-
     // The compiler will make its default constructor if not defined
     // A constructor needs to be public (at this stage of my learning, at least)
     Employee(string name, string company, int age) {
@@ -27,6 +50,10 @@ int main() {
 
     Employee employee2 = Employee("Jane Doe", "Google", 30);
     employee2.IntroduceYourself();
+
+    employee1.setAge(15);
+    employee1.setName("Alexander Beau Coup de Broglie");
+    std::cout << employee1.getName() << " is " << employee1.getAge() << " years old and works at " << employee1.getCompany();
 
     return 0;
 }
